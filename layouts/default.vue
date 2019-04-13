344e43c5d4a387473a07d7c6ef924ae40a8a6100
kou0402 <kou.4869.biz@gmail.com>
2019-04-14 02:10:54 +0900

    ajust layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,3 +3,14 @@
     <nuxt />
   </body>
 </template>
+
+<script>
+export default {
+  mounted() {
+    // ビューポートの高さを取得し、0.01を掛けて1%の値を算出して、vh単位の値を取得
+    const vh = window.innerHeight * 0.01
+    // カスタム変数--vhの値をドキュメントのルートに設定
+    document.documentElement.style.setProperty('--vh', `${vh}px`)
+  }
+}
+</script>
