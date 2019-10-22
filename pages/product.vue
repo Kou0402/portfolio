48375cc7c7f83c24b8629f0050ed4185d5f4884c
kou0402 <kou.4869.biz@gmail.com>
2019-10-22 16:39:59 +0900

    style: add product

--- pages/product.vue
+++ pages/product.vue
@@ -8,6 +8,12 @@
         :product-description="helloWorld.productDescription"
         :product-img="helloWorld.productImg"
       ></ProductCard>
+      <ProductCard
+        :product-url="portfolioLibrary.productUrl"
+        :product-name="portfolioLibrary.productName"
+        :product-description="portfolioLibrary.productDescription"
+        :product-img="portfolioLibrary.productImg"
+      ></ProductCard>
     </ContentsArea>
   </main>
 </template>
@@ -33,9 +39,23 @@ export default {
           'はバンド『BUMP OF CHICKEN』の『Butterflies』',
           'というアルバムの収録曲です。',
           'サイトを作成する際に何かテーマを決めて作ろうと思い',
-          '自分の好きなバンドのアルバムをテーマにしました。'
+          '自分の好きなバンドのアルバムをテーマにしました。',
+          '使用技術：Nuxt.js, Netlify'
         ],
         productImg: 'hello_world.png'
+      },
+      portfolioLibrary: {
+        productUrl: 'https://portfolio-library.com',
+        productName: 'Portfolio Library',
+        productDescription: [
+          'エンジニア・デザイナー向けのポートフォリオ共有',
+          'サイト。ポートフォリオを作成しようと思った時に',
+          '面白いポートフォリオサイトが沢山あることを知り',
+          'それらが眠っているだけではもったいないと感じ',
+          '共有できる場があればいいと思い作成しました。',
+          '使用技術：Nuxt.js, Firebase'
+        ],
+        productImg: 'portfolio_library.png'
       }
     }
   }
